a7b24a53286ef995213baeba1fd0e275e81b850c
Intec <Intec@PC215-10>
2021-12-13 13:31:14 +0100

    Add project files.

---
+++ TestLogica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLogica
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] left = new int[] { 1, 3, 2, 6, 9 };
+            int[] right = new int[] { 0, 0, 0, 0, 0 };
+            for (int i = 0; i < left.Length; i++)
+            {
+                Console.WriteLine(left[i]);
+            }
+
+            right =Swapping.ArraySwap(right);
+            left = Swapping.ArraySwap(left);
+
+            Console.WriteLine("changed the array");
+            for (int i = 0; i < left.Length; i++)
+            {
+                Console.WriteLine(left[i]);
+                Console.WriteLine(right[i]);
+            }
+
+        }
+    }
+}
---
+++ TestLogica/Swapping.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLogica
+{
+    public class Swapping
+    {
+        public static int[] ArraySwap(int[] right1)
+        {
+            int[] left = new int[] { 1, 3, 2, 6, 9 };
+            for (int i = 0; i < left.Length; i++)
+            {
+                right1[i] = left[i];
+            }
+            return right1;
+        }
+        public static int[] ArraySwapLeft(int[] left1)
+        {
+            int[] right = new int[] { 0, 0, 0, 0, 0 };
+            for (int i = 0; i < left1.Length; i++)
+            {
+                right[i] = left1[i];
+            }
+            return left1;
+        }
+    }
+}
